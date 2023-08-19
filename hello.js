ccde929707fb0d5af6ec57b5198e736affcc2722
elenamayag <142683768+elenamayag@users.noreply.github.com>
2023-08-19 16:46:51 -0500

    hello.js

--- hello.js
+++ hello.js
@@ -4,15 +4,15 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello");
+console.log ('Hi, World!   12   My name is Elena.  I am 23 years old.   One of my favorite quotes comes from an author name Louisa M. Alcott, "I am not afraid of stroms, for i am learning how to sail my ship." ')
 
-/*  
+/*  npm
 Next, PLAY! 
 Modify your code on line 7 to try to accomplish the tasks listed below:
 
    1. Change the message that is printed.
-   2. Figure out what the parentheses do. Will the code work without them?
-   3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
+   2. Figure out what the parentheses do. Will the code work without them? NO
+   3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")? no differnce but need one or the other
    4. Remove the semi-colon, ;.
    5. Print a number. (Bonus: Print two numbers added together).
    6. Print multiple messages one after the other.
